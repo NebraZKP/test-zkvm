627e06baaee4cd3ecb919a487160475ae5ea8fdb
Duncan Tebbs <dtebbs@users.noreply.github.com>
2024-04-04 18:30:18 +0000

    cuda flag, print cycle counts, simple G1 addition test

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ cargo install cargo-binstall
 cargo binstall cargo-risczero
 cargo risczero install
 ```
+
+# Run
+
+```console
+cd nebra0
+cargo run --release [--features=[cuda,metal]]
+```
--- nebra0/methods/guest/src/main.rs
+++ nebra0/methods/guest/src/main.rs
@@ -1,18 +1,23 @@
 #![no_main]
 // If you want to try std support, also update the guest Cargo.toml file
-#![no_std] // std support is experimental
+// #![no_std] // std support is experimental
 
-use ark_bn254::{Bn254, Fr, G1Affine, G2Affine};
-use ark_ec::{
-    pairing::{Pairing, PairingOutput},
-    AffineRepr,
-};
+use ark_bn254::{Bn254, G1Affine, G1Projective, G2Affine, G2Projective};
+use ark_ec::{pairing::Pairing, AffineRepr};
 use ark_ff::One;
 use risc0_zkvm::guest::env;
 use shared::{HasRepr, Inputs};
 // use serde::{Deserialize, Serialize};
 
 risc0_zkvm::guest::entry!(main);
+
+fn sum_refs(a: &G1Projective, b: &G1Projective) -> G1Projective {
+    a + b
+}
+
+fn sum_vals(a: G1Projective, b: G1Projective) -> G1Projective {
+    a + b
+}
 
 fn main() {
     // TODO: Implement your guest code here
@@ -41,24 +46,62 @@ fn main() {
 
     let inputs: Inputs = env::read();
 
-    let a1 = G1Affine::from_repr(&inputs.0);
-    let a2 = G2Affine::from_repr(&inputs.1);
-    let b1 = G1Affine::from_repr(&inputs.2);
-    let b2 = G2Affine::from_repr(&inputs.3);
+    // let x1 = env::cycle_count();
+    // let x2 = env::cycle_count();
 
-    // let ab1: G1Affine = (a1 + b1).into();
-    // env::commit(&ab1.to_repr());
+    let a1 = G1Affine::from_repr(&inputs.0).into_group();
+    // let x3 = env::cycle_count();
 
-    let multi_miller_result = Bn254::multi_miller_loop(&[a1, b1], &[a2, b2]);
-    let pairing_result = Bn254::final_exponentiation(multi_miller_result);
+    // let a2 = G2Affine::from_repr(&inputs.1);
+    // let x4 = env::cycle_count();
 
-    // Check pairing result
-    if let Some(target_field_value) = pairing_result {
-        if target_field_value.0 == <<Bn254 as Pairing>::TargetField as One>::one() {
-            env::exit(0)
-        }
+    let b1 = G1Affine::from_repr(&inputs.2).into_group();
+    // let x5 = env::cycle_count();
+
+    // let b2 = G2Affine::from_repr(&inputs.3);
+    // let x6 = env::cycle_count();
+
+    // Sum G1 points
+    {
+        let x6 = env::cycle_count();
+        // let ab1_vals = sum_vals(a1, b1);
+        let ab1_refs = sum_refs(&a1, &b1);
+        let x7 = env::cycle_count();
+
+        // let x8 = env::cycle_count();
+
+        // let ab1: G1Affine = ab1_vals.into();
+        let ab1: G1Affine = ab1_refs.into();
+
+        env::commit(&ab1.to_repr());
+        // let x9 = env::cycle_count();
+
+        // println!("cycles1: {x1}");
+        // println!("cycles2: {x2}");
+        // println!("cycles3: {x3}");
+        // println!("cycles4: {x4}");
+        // println!("cycles5: {x5}");
+        println!("cycles6: {x6}");
+        println!("cycles7: {x7}");
+        // println!("cycles8: {x8}");
+        // println!("cycles9: {x9}");
     }
-    env::exit(1);
+
+    // 2-pairing
+    // {
+    //     let multi_miller_result = Bn254::multi_miller_loop(&[a1, b1], &[a2, b2]);
+    //     let pairing_result = Bn254::final_exponentiation(multi_miller_result);
+
+    //     // Check pairing result
+    //     if let Some(target_field_value) = pairing_result {
+    //         env::commit(&target_field_value.0.c0.c0.c0.to_repr());
+
+    //         if target_field_value.0 == <<Bn254 as Pairing>::TargetField as One>::one() {
+    //             env::exit(0)
+    //         }
+    //     }
+    //     env::exit(1);
+    // }
 
     // env::log("f1_repr:");
     // env::write(f1.fmt());
