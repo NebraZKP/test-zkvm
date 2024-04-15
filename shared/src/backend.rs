a29a30c5e6954ccf31b25b777af9ad3ce703d792
Duncan Tebbs <dtebbs@users.noreply.github.com>
2024-04-15 15:07:27 +0000

    wip: basic add / subtract in plain Fq

--- shared/src/backend.rs
+++ shared/src/backend.rs
@@ -1,7 +1,5 @@
+use ark_ff::{BigInt, Field, Fp, Fp256, FpConfig, PrimeField, SqrtPrecomputation};
 use core::{marker::PhantomData, str::FromStr};
-
-use ark_bn254;
-use ark_ff::{BigInt, Fp, Fp256, FpConfig, PrimeField, SqrtPrecomputation};
 
 // #[derive(MontConfig)]
 // #[modulus = "21888242871839275222246405745257275088696311157297823662689037894645226208583"]
@@ -15,14 +13,52 @@ use ark_ff::{BigInt, Fp, Fp256, FpConfig, PrimeField, SqrtPrecomputation};
 //  13281191951274694749,
 //  3486998266802970665]
 
+type B = BigInt<4>;
+
 // Per-field properties, created by a macro
 pub trait Fp256PlainConfig: 'static + Sync + Send + Sized {
-    const MODULUS: BigInt<4>;
+    const MODULUS: B;
     const GENERATOR: Fp256<Fp256PlainBackend<Self>>;
     const ZERO: Fp256<Fp256PlainBackend<Self>>;
     const ONE: Fp256<Fp256PlainBackend<Self>>;
     const TWO_ADICITY: u32;
     const TWO_ADIC_ROOT_OF_UNITY: Fp256<Fp256PlainBackend<Self>>;
+
+    #[inline(always)]
+    fn __add_with_carry(a: &mut B, b: &B) -> bool {
+        use ark_ff::biginteger::arithmetic::adc_for_add_with_carry as adc;
+        let mut carry = 0;
+        carry = adc(&mut a.0[0], b.0[0], carry);
+        carry = adc(&mut a.0[1], b.0[1], carry);
+        carry = adc(&mut a.0[2], b.0[2], carry);
+        carry = adc(&mut a.0[3], b.0[3], carry);
+        carry != 0
+    }
+
+    #[inline(always)]
+    fn __sub_with_borrow(a: &mut B, b: &B) -> bool {
+        use ark_ff::biginteger::arithmetic::sbb_for_sub_with_borrow as sbb;
+        let mut borrow = 0;
+        borrow = sbb(&mut a.0[0], b.0[0], borrow);
+        borrow = sbb(&mut a.0[1], b.0[1], borrow);
+        borrow = sbb(&mut a.0[2], b.0[2], borrow);
+        borrow = sbb(&mut a.0[3], b.0[3], borrow);
+        borrow != 0
+    }
+
+    #[inline(always)]
+    fn __subtract_modulus(a: &mut Fp256<Fp256PlainBackend<Self>>) {
+        if a.is_geq_modulus() {
+            Self::__sub_with_borrow(&mut a.0, &Self::MODULUS);
+        }
+    }
+
+    #[inline(always)]
+    fn __subtract_modulus_with_carry(a: &mut Fp256<Fp256PlainBackend<Self>>, carry: bool) {
+        if a.is_geq_modulus() || carry {
+            Self::__sub_with_borrow(&mut a.0, &Self::MODULUS);
+        }
+    }
 }
 
 // Implementation based on a PlainFp256Config
@@ -65,27 +101,25 @@ impl<T: Fp256PlainConfig> FpConfig<4> for Fp256PlainBackend<T> {
     const SQRT_PRECOMP: Option<SqrtPrecomputation<Fp256<Self>>> = None;
 
     fn add_assign(a: &mut ark_ff::Fp<Self, 4>, b: &ark_ff::Fp<Self, 4>) {
-        use ark_ff::biginteger::arithmetic::adc_for_add_with_carry as adc;
-        let mut carry = 0;
-        carry = adc(&mut a.0 .0[0], b.0 .0[0], carry);
-        carry = adc(&mut a.0 .0[1], b.0 .0[1], carry);
-        carry = adc(&mut a.0 .0[2], b.0 .0[2], carry);
-        adc(&mut a.0 .0[3], b.0 .0[3], carry);
+        T::__add_with_carry(&mut a.0, &b.0);
+        T::__subtract_modulus(a);
     }
-    fn sub_assign(_: &mut ark_ff::Fp<Self, 4>, _: &ark_ff::Fp<Self, 4>) {
-        todo!()
+    fn sub_assign(a: &mut ark_ff::Fp<Self, 4>, b: &ark_ff::Fp<Self, 4>) {
+        // If `other` is larger than `self`, add the modulus to self first.
+        if b.0 > a.0 {
+            T::__add_with_carry(&mut a.0, &Self::MODULUS);
+        }
+        T::__sub_with_borrow(&mut a.0, &b.0);
     }
     fn double_in_place(_: &mut ark_ff::Fp<Self, 4>) {
         todo!()
     }
     fn neg_in_place(a: &mut ark_ff::Fp<Self, 4>) {
-        // use ark_ff::biginteger::arithmetic::sbb_for_sub_with_borrow as sbb;
-        // let mut borrow = 0;
-        // borrow = sbb(&mut a.0[0], b.0[0], borrow);
-        // borrow = sbb(&mut a.0[1], b.0[1], borrow);
-        // borrow = sbb(&mut a.0[2], b.0[2], borrow);
-        // sbb(&mut a.0[3], b.0[3], borrow);
-        todo!()
+        if *a != Fp::<Self, 4>::ZERO {
+            let mut tmp = Self::MODULUS;
+            T::__sub_with_borrow(&mut tmp, &a.0);
+            a.0 = tmp;
+        }
     }
     fn mul_assign(_: &mut ark_ff::Fp<Self, 4>, _: &ark_ff::Fp<Self, 4>) {
         todo!()
@@ -154,20 +188,56 @@ impl Fp256PlainConfig for FqPlainConfig {
 
 pub type Fq = Fp256<Fp256PlainBackend<FqPlainConfig>>;
 
-#[test]
-fn test_fq() {
-    let x = BigInt::new([12341234134, 431243124321, 987698769876, 678968976789]);
-    let y = BigInt::new([12341234134, 431243124321, 987698769876, 678968976789]);
+#[cfg(test)]
+mod test {
+    use super::*;
+    use ark_bn254;
 
-    let x_fq = Fq::from_bigint(x).unwrap();
-    let y_fq = Fq::from_bigint(y).unwrap();
+    #[test]
+    fn test_fq() {
+        let x = BigInt::new([12341234134, 431243124321, 987698769876, 678968976789]);
+        let y = BigInt::new([12341234134, 431243124321, 987698769876, 678968976789]);
 
-    let x_ark_fq = ark_bn254::Fq::from_bigint(x).unwrap();
-    let y_ark_fq = ark_bn254::Fq::from_bigint(y).unwrap();
+        let x_fq = Fq::from_bigint(x).unwrap();
+        let y_fq = Fq::from_bigint(y).unwrap();
 
-    {
-        let x_plus_y = x_fq + y_fq;
+        let x_ark_fq = ark_bn254::Fq::from_bigint(x).unwrap();
+        let y_ark_fq = ark_bn254::Fq::from_bigint(y).unwrap();
+
         let expect_x_plus_y = x_ark_fq + y_ark_fq;
-        assert_eq!(expect_x_plus_y.into_bigint(), x_plus_y.into_bigint());
+        let expect_x_minus_y = x_ark_fq - y_ark_fq;
+
+        {
+            let x_plus_y = x_fq + y_fq;
+            assert_eq!(expect_x_plus_y.into_bigint(), x_plus_y.into_bigint());
+        }
+
+        {
+            let mut x_plus_y = x_fq;
+            x_plus_y += y_fq;
+            assert_eq!(expect_x_plus_y.into_bigint(), x_plus_y.into_bigint(),);
+        }
+
+        {
+            let x_minus_y = x_fq - y_fq;
+            assert_eq!(expect_x_minus_y.into_bigint(), x_minus_y.into_bigint(),);
+        }
+
+        {
+            let mut x_minus_y = x_fq;
+            x_minus_y -= y_fq;
+            assert_eq!(expect_x_minus_y.into_bigint(), x_minus_y.into_bigint(),);
+        }
+
+        {
+            let mut y_minus_x = y_fq;
+            y_minus_x -= x_fq;
+            assert_eq!((-expect_x_minus_y).into_bigint(), y_minus_x.into_bigint(),);
+        }
+
+        assert_eq!(
+            (-ark_bn254::Fq::from(7) + ark_bn254::Fq::from(1)).into_bigint(),
+            (-Fq::from(7) + Fq::from(1)).into_bigint()
+        );
     }
 }
